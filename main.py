0037e5585a868ea8bc2bae10d2f1583cb371ffbb
Kevin Booth <kevin.booth@insight.com>
2020-04-03 18:38:49 -0400

    updates

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from random import randint
-
-from PIL import Image, ImageDraw
-
-
-class PuzzlePiece(object):
-
-    def __init__(self, number_of_balls, box_dimensions, identifier):
-        self.number_of_balls = number_of_balls
-        self.box_dimensions = box_dimensions
-        self.identifier = identifier
+import numpy
+import cv2
 
 piece_color_map = {
-    'A': (255,165,0),
+    'A': (255, 165, 0),
     'B': (255, 0, 0),
     'C': (0, 0, 255),
-    'D': (255,204,255),
+    'D': (255, 204, 255),
     'E': (0, 255, 0),
-    'F': (255,255,255),
+    'F': (255, 255, 255),
     'G': (155, 255, 255),
     'H': (153, 0, 153),
     'I': (255, 255, 0),
@@ -26,11 +18,32 @@ piece_color_map = {
     'X': (0, 0, 0)
 }
 
+resize_multiplier = 50
+
+class PuzzlePiece(object):
+
+    def __init__(self, identifier, piece_dimensions, piece_definition):
+        self.piece_dimensions = piece_dimensions
+        self.width = piece_dimensions[0]
+        self.height = piece_dimensions[1]
+        self.piece_definition = piece_definition
+        self.identifier = identifier
+
+    def draw(self):
+        image = numpy.zeros((self.piece_dimensions[0] * resize_multiplier, self.piece_dimensions[1] * resize_multiplier, 3), numpy.uint8)
+
+        img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imshow('test', img2)
+        cv2.waitKey(250)
+
+
+
 pieces = {}
-p = PuzzlePiece(4, [2, 3], 'A')
+p = PuzzlePiece('A', (2, 3), ((1, 1), (1, 0), (1, 1)))
+p.draw()
 pieces[p.identifier] = p
 
-resize_multiplier = 50
+
 game_width = 11
 game_height = 5
 
@@ -42,33 +55,36 @@ with open("level6-11.txt") as textFile:
     lines = [line.split() for line in textFile]
     print(lines)
 
-image = Image.new(mode='RGB', size=(width, height), color=255)
+def draw_rect(x, y, resize_multiplier, color, img):
+    start_point = (x, y)
+    end_point = (x + resize_multiplier, y + resize_multiplier)
+    return cv2.rectangle(img, start_point, end_point, color, -1)
 
 
-def draw_rect(x, y, resize_multiplier, color):
-    shape = (x, y, x + resize_multiplier, y + resize_multiplier)
-    img1 = ImageDraw.Draw(image)
-    img1.rectangle(shape, fill=color)
-
-
-def render_board():
+def render_board(img):
     x = 0
     y = 0
     for w in range(0, game_width):
-        for l in range(0, game_height):
+        for h in range(0, game_height):
+            game_board_row = lines[h][0]
+            piece_code = game_board_row[w]
 
-            game_board_row = lines[l]
-            piece_code = game_board_row[0][w]
-
-            draw_rect(x, y, resize_multiplier, piece_color_map[piece_code])
+            img = draw_rect(x, y, resize_multiplier, piece_color_map[piece_code], img)
             y += resize_multiplier
+
+            img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow('test', img2)
+            cv2.waitKey(10)
 
         x += resize_multiplier
         y = 0
 
-    image.show()
+    return img
 
 
+image = numpy.zeros((height, width, 3), numpy.uint8)
 
-
-render_board()
+image = render_board(image)
+img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+cv2.imshow('test', img)
+cv2.waitKey(0)
